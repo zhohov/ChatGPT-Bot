f87d2dfb6017aee6e79c5a3bfe988e4d04ed9c21
maksimzhohov <zhohovmaksim@gmail.com>
2023-04-05 00:58:02 +0300

    Added ChatGPT response

--- bot/handlers/user.py
+++ bot/handlers/user.py
@@ -1,9 +1,10 @@
+import logging
 import os
 
+import openai
 from aiogram import types, Router, F
 from aiogram.enums import ContentType
 from aiogram.filters import Command, Text
-from aiogram.types import Message
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from sqlalchemy.orm import sessionmaker
 
@@ -13,15 +14,11 @@ from bot.database.user import get_subscription_end_date, get_requests
 router = Router()
 
 
-async def start_command(message: types.Message) -> Message:
+async def start_command(message: types.Message) -> types.Message:
     return await message.answer('Start')
 
 
-async def request_command(message: types.Message) -> Message:
-    return await message.answer('Request!')
-
-
-async def buy_command(message: types.Message) -> Message:
+async def buy_command(message: types.Message) -> types.Message:
     builder = InlineKeyboardBuilder()
     builder.add(types.InlineKeyboardButton(
         text='Buy',
@@ -66,11 +63,22 @@ async def profile_command(message: types.Message, session_maker: sessionmaker) -
         await message.answer(f'Профиль\n\nid: {message.chat.id}\nПодписка: нет\nЗапросы:  {requests}/{10}')
 
 
+async def gpt_response(message: types.Message) -> None:
+    openai.api_key = os.getenv('opneai_token_api')
+    response = openai.ChatCompletion.create(
+        # model="text-davinci-003",
+        model="gpt-3.5-turbo",
+        messages=[{'role': 'user', 'content': message.text}]
+    )
+    await message.answer(response['choices'][0]['message']['content'])
+    logging.info(response)
+
+
 def register_user_handlers(router: Router) -> None:
     router.message.register(start_command, Command(commands=['start']), flags={'registration_check': 'registration_check'})
-    router.message.register(request_command, Command(commands=['request']), flags={'update_requests': 'update_requests'})
     router.message.register(profile_command, Command(commands=['profile']))
     router.message.register(buy_command, Command(commands=['buy']))
     router.callback_query.register(buy_send_invoice, Text('buy'))
     router.pre_checkout_query.register(pre_checkout)
     router.message.register(successful_pay, F.content_type == ContentType.SUCCESSFUL_PAYMENT)
+    router.message.register(gpt_response, flags={'update_requests': 'update_requests'})
